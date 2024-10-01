12c18f02add99cb7cbfce497ec38908f251af6df
CedricDeVon <vaughncedriclipardoaraneta@gmail.com>
2024-10-02 07:22:15 +0800

    Implemented base HTML and CSS contents

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,16 @@ export default {
   content: ['./public/*.html'],
   theme: {
     extend: {
+      colors: {
+        "light-1": '#D4D5E9',
+        "light-2": '#6B6C7B',
+        "dark-1": '#030415',
+        "dark-2": '#0C0D1D',
+        "dark-3": '#171832',
+        "main-1": '#24F143',
+        "main-2": '#05742A',
+        "main-3": '#9124F1',
+      }
     },
   },
   plugins: [],
